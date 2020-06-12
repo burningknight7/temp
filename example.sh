e76220f9f5fe183315dba8e303b0219b136abf25
affirmed <affirmed@qa-ems-docker-01.affirmed.local>
2020-06-12 03:28:47 -0400

    Addition almost done

--- example.sh
+++ example.sh
@@ -1,17 +1,5 @@
-extract_name(){
-line=$1
-echo $line >&2
-name1=`echo $line| awk -F \' '{print $2}'`
-echo $name1 >&2
-name2= $(echo $line| awk -F \" '{print $2}')
-echo $name2 >&2
-if [ "$name1" = "" ]; then
-	echo "$name2"
-else
-	echo "$name1"
-fi
-}
-
-line=" - job_name: 'example' "
-name=$(extract_name "$line")
-echo "name is "$name
+count="2"
+for i in $( seq 1 $count )
+do
+	echo "$i"
+done
